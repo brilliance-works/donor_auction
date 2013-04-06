5869d40af7715460e891db14a0b6de3b010cecc0
Brilliance <info@brilliance-works.com>
2013-04-06 11:52:52 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DonorAuction::Application.config.secret_token = '7574d98747c25ae3e1cf22aa9a2daca9382ca3a1bad36ecc922b1a02d7a92e0d8c3204e69a8a669806f695c77fc12df95036d2b37ade6c9521f2b2406b54d5a7'
